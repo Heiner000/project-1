a6fc7aa7ab22b0e67f060554f89431f860901802
Heiner000 <garrett.heiner@gmail.com>
2023-03-17 15:14:10 -0700

    fleshed out css... needs work. started js, start button dom changes to gameplay screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-console.log("welcome to the project")
+console.log("welcome to the bridge troll toll project")
+
+const startBtn = document.querySelector("#start-btn")
+const instructionsBtn = document.querySelector("#instructions-btn")
+const titleCard = document.querySelector("#title-card")
+const gameCard = document.querySelector("#game-card")
+const promptDiv = document.querySelector("#prompt-div")
+const choicesDiv = document.querySelector("#choices-div")
+
+console.log(startBtn, instructionsBtn)
+
+startBtn.addEventListener("click", () => {
+    titleCard.style.display = "none"
+    gameCard.style.display = "grid"
+    promptDiv.style.display = "flex"
+    choicesDiv.style.display = "grid"
+})
