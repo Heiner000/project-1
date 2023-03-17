d0250cb370f426e2b9d84b467bedd237fbd19557
Heiner000 <garrett.heiner@gmail.com>
2023-03-17 16:25:53 -0700

    working out the javascript a bit

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,29 @@ const choicesDiv = document.querySelector("#choices-div")
 
 console.log(startBtn, instructionsBtn)
 
+const riddle = [
+    {
+        prompt: "You measure my life in hours and I serve you by expiring. I'm quick when I'm thin and slow when I'm fat. The wind is my enemy. What am I",
+        choices: ["Candle","Hourglass","Caterpillar","Battery"],
+        answer: "Candle"
+    }
+]
+
+let riddleIndex
+
+function displayRiddle(riddle) {
+    promptDiv.innerText = riddle.prompt
+    let choiceBtns = document.querySelectorAll(".choice-btn")
+    for (let i = 0; i < riddle.choices.length; i++) {
+        choiceBtns[i].innerText = riddle.choices[i]
+    }
+}
+
+
 startBtn.addEventListener("click", () => {
     titleCard.style.display = "none"
     gameCard.style.display = "grid"
     promptDiv.style.display = "flex"
     choicesDiv.style.display = "grid"
+    displayRiddle()
 })
