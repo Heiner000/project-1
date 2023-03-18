88178a2566b0d2c536fe4dbdf02457c0aa49d403
Heiner000 <garrett.heiner@gmail.com>
2023-03-17 18:15:01 -0700

    response buttons working, console feedback. outlined missing pieces

--- js/scripts.js
+++ js/scripts.js
@@ -7,25 +7,58 @@ const gameCard = document.querySelector("#game-card")
 const promptDiv = document.querySelector("#prompt-div")
 const choicesDiv = document.querySelector("#choices-div")
 
-console.log(startBtn, instructionsBtn)
+const choice1 = document.querySelector("#choice-1")
+const choice2 = document.querySelector("#choice-2")
+const choice3 = document.querySelector("#choice-3")
+const choice4 = document.querySelector("#choice-4")
+
+let riddlesIndex = 0
 
 startBtn.addEventListener("click", () => {
     titleCard.style.display = "none"
     gameCard.style.display = "grid"
     promptDiv.style.display = "flex"
     choicesDiv.style.display = "grid"
-    displayRiddle(riddle[0])
+    displayRiddle(riddles[0])
 })
 
-const riddle = [
+choice1.addEventListener("click", handleChoiceClick)
+choice2.addEventListener("click", handleChoiceClick)
+choice3.addEventListener("click", handleChoiceClick)
+choice4.addEventListener("click", handleChoiceClick)
+
+const riddles = [
     {
         prompt: "You measure my life in hours and I serve you by expiring. I'm quick when I'm thin and slow when I'm fat. The wind is my enemy. What am I",
         choices: ["Candle","Hourglass","Caterpillar","Battery"],
         answer: "Candle"
+    },
+    {
+        prompt: "I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but I consume all that surrounds me. What am I?",
+        choices: ["Fire","Rust", "Crystal","River"],
+        answer: "Fire"
+    },
+    {
+        prompt: "I am light as a feather, yet the strongest man cannot hold me fo rmuch more than a moment. What am I?",
+        choices: ["Thought","Time","Idea","Breath"],
+        answer: "Breath"
     }
 ]
 
-let riddleIndex
+function handleChoiceClick(e) {
+    const userChoice = e.target.innerText;
+    const currentRiddle = riddles[riddlesIndex]
+
+    if (userChoice === currentRiddle.answer) {
+        console.log("GOOD JOB - YOU GOT IT RIGHT")
+    } else {
+        console.log("OOPS, YOU'RE TROLL FOOD")
+    }
+    riddlesIndex++
+    console.log(riddlesIndex)
+    // need a feedback message
+    // need to advance to next question
+}
 
 function displayRiddle(riddle) {
     promptDiv.innerText = riddle.prompt
@@ -35,4 +68,8 @@ function displayRiddle(riddle) {
     }
 }
 
+// need a function to decrement Hump's limbs on wrong answers.
+    // maybe we want to add a few limbs to allow for wrong answers on multiple questions
 
+// need gameover screens, both winner & loser
+    // need reset button
