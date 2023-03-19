d94422624fe39cfcd995353ac746b37cc307ef9e
Heiner000 <garrett.heiner@gmail.com>
2023-03-18 19:34:46 -0700

    checkAnswer function still wip, mostly done

--- js/scripts.js
+++ js/scripts.js
@@ -14,8 +14,26 @@ const choice2 = document.querySelector("#choice-2")
 const choice3 = document.querySelector("#choice-3")
 const choice4 = document.querySelector("#choice-4")
 
-
+const riddles = [
+    {
+        prompt: "You measure my life in hours and I serve you by expiring. I'm quick when I'm thin and slow when I'm fat. The wind is my enemy. What am I",
+        choices: ["Candle","Hourglass","Caterpillar","Battery"],
+        answer: "Candle"
+    },
+    {
+        prompt: "I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but I consume all that surrounds me. What am I?",
+        choices: ["Fire","Rust", "Crystal","River"],
+        answer: "Fire"
+    },
+    {
+        prompt: "I am light as a feather, yet the strongest man cannot hold me fo rmuch more than a moment. What am I?",
+        choices: ["Thought","Time","Idea","Breath"],
+        answer: "Breath"
+    }
+]
 let riddlesIndex = 0
+const currentRiddle = riddles[riddlesIndex]
+
 
 startBtn.addEventListener("click", () => {
     console.log("That sounded like the click of a start button")
@@ -39,41 +57,45 @@ choice2.addEventListener("mousedown", handleChoiceClick)
 choice3.addEventListener("mousedown", handleChoiceClick)
 choice4.addEventListener("mousedown", handleChoiceClick)
 
-const riddles = [
-    {
-        prompt: "You measure my life in hours and I serve you by expiring. I'm quick when I'm thin and slow when I'm fat. The wind is my enemy. What am I",
-        choices: ["Candle","Hourglass","Caterpillar","Battery"],
-        answer: "Candle"
-    },
-    {
-        prompt: "I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but I consume all that surrounds me. What am I?",
-        choices: ["Fire","Rust", "Crystal","River"],
-        answer: "Fire"
-    },
-    {
-        prompt: "I am light as a feather, yet the strongest man cannot hold me fo rmuch more than a moment. What am I?",
-        choices: ["Thought","Time","Idea","Breath"],
-        answer: "Breath"
-    }
-]
-
 function handleChoiceClick(e) {
     const userChoice = e.target.innerText;
-    const currentRiddle = riddles[riddlesIndex]
     console.log("I heard a choice click")
     
     // create a checkAnswer function
+    const isCorrect = checkAnswer(userChoice, currentRiddle)
+    console.log("is correct?: ", isCorrect)
+    
+    console.log("riddlesIndex: ", riddlesIndex)
+}
+
+function checkAnswer(userChoice) {
+
     if (userChoice === currentRiddle.answer) {
         console.log("GOOD JOB - YOU GOT IT RIGHT")
+        // need a feedback message
+        choicesDiv.style.display = "none"
+        const feedbackDiv = document.querySelector("#feedback-div")
+        feedbackDiv.style.display = "flex"
+        feedbackDiv.innerText = "Good jorb, we'll see if you can do it again..."
         // need to advance to next question
-        
+        const nextRiddleBtn = document.createElement("button")
+        nextRiddleBtn.id = "next-riddle-btn"
+        feedbackDiv.append(nextRiddleBtn)
+        if (riddlesIndex === riddles.length -1) {
+            // *** need a reset button event listener
+            nextRiddleBtn.innerText = "Reset"
+        } else {
+            // ***
+            nextRiddleBtn.innerText = "Next Riddle"
+        }
         riddlesIndex++
+        
     } else {
+        // *** I want to append a lost limb message to prompt
+            // [stretch goal: create an array of troll trolling phrases to cycle through]
         console.log("OOPS, LoSt A LiMb!!")
         removeLimb()
     }
-    console.log("riddlesIndex: ", riddlesIndex)
-    // need a feedback message
 }
 
 function displayRiddle(riddle) {
@@ -96,3 +118,19 @@ function removeLimb() {
 
 // need gameover screens, both winner & loser
     // need reset button
+
+
+/*
+const feedbackDiv = document.createElement("div")
+const nextRiddleBtn = document.createElement("button")
+nextRiddleBtn.innerText = "Next Riddle"
+feedbackDiv.appendChild(nextRiddleBtn)
+
+const feedbackDiv = document.querySelector("#feedback-div")
+feedbackDiv.style.display = "block"
+
+nextRiddleBtn.addEventListener("click", () => {
+    feedbackDiv.style.display = "none"
+    displayRiddle()
+})
+*/
